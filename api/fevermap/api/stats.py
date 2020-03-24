1d86c0d87572b56a4869b953497734ac601198b0
Otto Kekäläinen <otto@kekalainen.net>
2020-03-24 15:20:27 +0200

    API: If database is empty, return a all zeros stats response
    This fixes the error:
    
      File "./fevermap/api/stats.py", line 29, in get
        'since': db_session.query(Submitter).order_by(Submitter.timestamp_created).limit(1).one().timestamp_created.isoformat(timespec='seconds'),
      File "/usr/lib/python3/dist-packages/sqlalchemy/orm/query.py", line 3046, in one
        raise orm_exc.NoResultFound("No row was found for one()")
        sqlalchemy.orm.exc.NoResultFound: No row was found for one()

--- api/fevermap/api/stats.py
+++ api/fevermap/api/stats.py
@@ -21,22 +21,46 @@ class StatsResource(Resource):
 
         app.logger.info('Serving stats')
 
+        data = {
+            'submitters': {
+                'since': None,
+                'total': 0,
+                'today': 0,
+                'past_7_days': 0,
+            },
+            'submissions': {
+                'since': None,
+                'total': 0,
+                'today': 0,
+                'past_7_days': 0,
+            }
+        }
+
+        # First check if the database is empty (e.g new development install)
+        # and bail out quickly with all zeros response in that case
+        submitters_count = db_session.query(Submitter).count()
+
+        if submitters_count == 0:
+            return {
+                'success': True,
+                'data': data
+            }
+
         midnight = datetime.combine(datetime.now(), datetime.min.time())
         midnight_7_days_ago = midnight - timedelta(days=7)
 
-        data = {
-            'submitters': {
-                'since': db_session.query(Submitter).order_by(Submitter.timestamp_created).limit(1).one().timestamp_created.isoformat(timespec='seconds'),
-                'total': db_session.query(Submitter).count(),
-                'today': db_session.query(Submitter).filter(Submitter.timestamp_modified > midnight).count(),
-                'past_7_days': db_session.query(Submitter).filter(Submitter.timestamp_modified > midnight_7_days_ago).count(),
-            },
-            'submissions': {
+        data['submitters'] = {
+            'since': db_session.query(Submitter).order_by(Submitter.timestamp_created).limit(1).one().timestamp_created.isoformat(timespec='seconds'),
+            'total': submitters_count,
+            'today': db_session.query(Submitter).filter(Submitter.timestamp_modified > midnight).count(),
+            'past_7_days': db_session.query(Submitter).filter(Submitter.timestamp_modified > midnight_7_days_ago).count(),
+        }
+
+        data['submissions'] = {
                 'since': db_session.query(Submission).order_by(Submission.timestamp_created).limit(1).one().timestamp_created.isoformat(timespec='seconds'),
                 'total': db_session.query(Submission).count(),
                 'today': db_session.query(Submission).filter(Submission.timestamp_modified > midnight).count(),
                 'past_7_days': db_session.query(Submission).filter(Submission.timestamp_modified > midnight_7_days_ago).count(),
-            }
         }
 
         return {
