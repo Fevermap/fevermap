2a41abbdaa25d1dffff7a4cf8bbc0909ced4a6fd
Ilkka Tengvall <ikke@iki.fi>
2020-04-01 17:26:30 +0300

    update ocp confs and utility scripts

---
+++ ocp/production/ensure-encrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Encrypting files, it's fine if you see error about already encrypted files."
+
+for file in route-prod-fevermap-api.yaml \
+route-prod-fevermap-front.yaml \
+secret-prod-aws-db-backup.yaml \
+secret-prod-fevermap-api-db.yaml \
+secret-prod-fevermap-db.yaml; \
+do
+  echo encrypting $file
+  ansible-vault encrypt --vault-password-file=../.vault-pw $file
+done
