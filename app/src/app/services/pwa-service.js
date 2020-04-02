a271f9e27eb64daa7ef1986e9a0ace9b51925a38
Matsuuu <huhta.matias@gmail.com>
2020-04-02 10:16:51 +0300

    Add install prompt for iOS devices

--- app/src/app/services/pwa-service.js
+++ app/src/app/services/pwa-service.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import Dialog from '../components/dialog.js';
 import Translator from '../util/translator.js';
 
@@ -9,12 +10,28 @@ export default class PWAService {
     PWAService._instance = new PWAService(deferredPrompt);
   }
 
+  /**
+   * PWAService._instance is initialized, if a install prompt has
+   * been caught and deferred.
+   *
+   * However on iOS, this prompt is not launched, so we need to check on iOS devices
+   * if the device is an iOS device and if it's already running PWA
+   *
+   * @returns Is the page installable as a PWA
+   */
   static installable() {
-    return typeof PWAService._instance !== 'undefined';
+    return (
+      typeof PWAService._instance !== 'undefined' ||
+      (PWAService.isIos() && !PWAService.isInStandaloneMode())
+    );
   }
 
   static launchInstallDialog() {
     if (!PWAService._instance) {
+      return;
+    }
+    if (PWAService.isIos()) {
+      PWAService._instance.openIosDialog();
       return;
     }
     PWAService._instance.openInstallDialog();
@@ -38,5 +55,21 @@ export default class PWAService {
     document.addEventListener('pwa-install-approve', () => {
       this.deferredPrompt.prompt();
     });
+  }
+
+  openIosDialog() {
+    const iosBanner = document.createElement('ios-pwa-install-prompt');
+    document.body.append(iosBanner);
+  }
+
+  // Detects if the device is a iOS device
+  static isIos() {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+  }
+
+  // Detects if the device is in standalone more already
+  static isInStandaloneMode() {
+    return 'standalone' in window.navigator && window.navigator.standalone;
   }
 }
