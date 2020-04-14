fdbea8ff01fb28496c8dfafe36490004ea53d0cf
Otto Kekäläinen <otto@kekalainen.net>
2020-04-15 01:30:06 +0300

    Add country parameter to API endpoint 'stats'

--- api/fevermap/api/stats.py
+++ api/fevermap/api/stats.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 
 from flask_restful import Resource
+from flask import request
 
 from fevermap.db import db_session
 from fevermap.db.models.submission import Submission
@@ -13,13 +14,25 @@ app = Flask('fevermap')
 
 
 class StatsResource(Resource):
+
     def get(self, **kwargs):
         """Get stats."""
-
         # @TODO: Add cache headers so Nginx will cache the respons and not
         # hit the Flask server for all requests
 
-        app.logger.info('Serving stats')
+        # Define filter array. Empty by default, which means no filtering.
+        filters = []
+
+        # If request has "?country=FI" then stats will apply only to submission
+        # data with location_country_code="FI". Otherwise server global stats.
+        if 'country' in request.args:
+            app.logger.info(
+                'Serving stats for country %s' % request.args['country'])
+            filters.append(
+                Submission.location_country_code.__eq__(
+                    request.args['country']))
+        else:
+            app.logger.info('Serving stats')
 
         data = {
             'generated': datetime.now().isoformat(timespec='seconds'),
@@ -39,9 +52,10 @@ class StatsResource(Resource):
 
         # First check if the database is empty (e.g new development install)
         # and bail out quickly with all zeros response in that case
-        submitters_count = db_session.query(Submitter).count()
+        submission_count = db_session.\
+            query(Submission).filter(*filters).count()
 
-        if submitters_count == 0:
+        if submission_count == 0:
             return {
                 'success': True,
                 'data': data
@@ -50,18 +64,65 @@ class StatsResource(Resource):
         midnight = datetime.combine(datetime.now(), datetime.min.time())
         midnight_7_days_ago = midnight - timedelta(days=7)
 
+        # Analyze submissions, but group by submitter id to see how many unique
+        # submitters submitted
         data['submitters'] = {
-            'since': db_session.query(Submitter).order_by(Submitter.timestamp_created).limit(1).one().timestamp_created.isoformat(timespec='seconds'),
-            'total': submitters_count,
-            'today': db_session.query(Submitter).filter(Submitter.timestamp_modified > midnight).count(),
-            'past_7_days': db_session.query(Submitter).filter(Submitter.timestamp_modified > midnight_7_days_ago).count(),
+            # When was the first submitter created
+            'since':
+                db_session.query(Submitter).
+                outerjoin(Submission, Submitter.id == Submission.submitter_id).
+                filter(*filters).
+                order_by(Submitter.timestamp_created).
+                limit(1).
+                one().
+                timestamp_created.isoformat(timespec='seconds'),
+            # How many submitters have submitted in total
+            'total':
+                db_session.query(Submission).
+                filter(*filters).
+                group_by(Submission.submitter_id).
+                count(),
+            # How many submitters have submitted today
+            'today':
+                db_session.query(Submission).
+                filter(*filters).
+                filter(Submission.timestamp_modified > midnight).
+                group_by(Submission.submitter_id).
+                count(),
+            # How many submitters have submitted in past week
+            'past_7_days':
+                db_session.query(Submission).
+                filter(*filters).
+                filter(Submission.timestamp_modified > midnight_7_days_ago).
+                group_by(Submission.submitter_id).
+                count(),
         }
 
         data['submissions'] = {
-            'since': db_session.query(Submission).order_by(Submission.timestamp_created).limit(1).one().timestamp_created.isoformat(timespec='seconds'),
-            'total': db_session.query(Submission).count(),
-            'today': db_session.query(Submission).filter(Submission.timestamp_modified > midnight).count(),
-            'past_7_days': db_session.query(Submission).filter(Submission.timestamp_modified > midnight_7_days_ago).count(),
+            # When was the first submission created
+            'since':
+                db_session.query(Submission).
+                filter(*filters).
+                order_by(Submission.timestamp_created).
+                limit(1).
+                one().timestamp_created.isoformat(timespec='seconds'),
+            # How many submission has there been in total
+            'total':
+                db_session.query(Submission).
+                filter(*filters).
+                count(),
+            # How many submission has there been today
+            'today':
+                db_session.query(Submission).
+                filter(*filters).
+                filter(Submission.timestamp_modified > midnight).
+                count(),
+            # How many submission has there been in past week
+            'past_7_days':
+                db_session.query(Submission).
+                filter(*filters).
+                filter(Submission.timestamp_modified > midnight_7_days_ago).
+                count(),
         }
 
         return {
